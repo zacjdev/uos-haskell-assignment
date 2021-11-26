257fa2f67576d5da0d4b991239224786edfca0b7
Zac Jones <63209111+zacjdev@users.noreply.github.com>
2021-11-26 19:28:26 +0000

    toFoundations

--- a.hs
+++ a.hs
@@ -7,6 +7,7 @@ stack ghci --package random
 -- Step 1: Initial Datatypes
 import System.Random
 import Data.List
+import Data.Bool (Bool)
 
 data Suit = Hearts | Diamonds | Clubs | Spades
   deriving (Eq, Show, Ord, Enum, Bounded)
@@ -47,99 +48,53 @@ type Foundations = [Deck]
 
 type Columns = [Deck]
 
-type Reserves = [Card]
+type Reserves = Deck
 
 newtype Board = Board EOBoard-- | Board (SBoard)
   deriving (Show)
 
-newtype EOBoard = EOBoard (Foundations, Columns, Reserves)
+type EOBoard = (Foundations, Columns, Reserves)
 
-instance Show EOBoard where
-  show (EOBoard (fs, cs, rs)) = "Foundations: " ++ show fs ++ "\nColumns: " ++ show cs ++ "\nReserves: " ++ show rs
+compBoard :: EOBoard
+compBoard = ([[(s, p) | s <- [Hearts], p <- reverse [Ace .. King]], [(s, p) | s <- [Diamonds], p <- reverse [Ace .. King]], [(s, p) | s <- [Spades], p <- reverse [Ace .. King]], [(s, p) | s <- [Clubs], p <- reverse [Ace .. King]]], [], [])
 
+testBoard :: EOBoard
+testBoard = ([[(Hearts, Ace)],[],[],[]], [], [(Hearts, Two)])
 
 -- Step 4: Implement further functionality
 eODeal :: Int -> EOBoard
-eODeal n = EOBoard (foundations, columns, reserves)
+eODeal n = (foundations, columns, reserves)
   where
     deck = shuffle n pack
-    foundations = [[],[],[],[]]
+    foundations = []
     reserves = take 4 deck
     makeCols :: [Card] -> [Deck]
     -- split deck into decks of 6 cards
     makeCols xs = if length xs < 6 then [] else take 6 xs : makeCols (drop 6 xs)
     columns = makeCols deck
 
+
+
+
+findAces :: EOBoard -> Deck
+findAces (fs,cs,rs) = filter isAce ((map head (filter (not.null) cs)) ++ rs) -- COPIED CODE!!!!!
+
+findSuccs :: EOBoard -> Deck
+findSuccs (fs,cs,rs) = filter (\f -> f `elem` ((map head (filter (not.null) cs)) ++ rs)) (filter (not.null) (map sCard (map (\f -> last f) fs))) -- COPIED CODE!!!!!
+
 toFoundations :: EOBoard -> EOBoard
-toFoundations board@(fs, c:cs, rs)
-  | foundationValid tail c fs = True
+toFoundations board
+  | (not.null) (findAces board) = toFoundations(moveToFoundation(head (findAces board)) board)
+  | (not.null) (findSuccs board) = toFoundations(moveToFoundation(head (findSuccs board)) board)
+  | otherwise = board
 
-foundationValid :: Card -> Foundations -> Bool
-foundationValid card (f:fs)
-  | null f = False
-  | isAce card = True
-  | card == sCard(last f) = True
-  | otherwise = foundationValid card fs
-
-
---stack ghci --package random
-
-{-
--- foundation will add new elements to the end. i0 = ace
-foundationValid :: Card -> Foundation -> Bool
-foundationValid c fs
-  | null c = False
-  | isAce c = True
-  | c `elem` filter fs (sCard . head) = True
-  | otherwise  = False
-
-findCardsToMove :: EOBoard -> Deck
-findCardsToMove (fs, cs, rs) =
-  filter foundationValid fs (filter (not.null) (map head columns ++ rs))
-
-
-
--}
-
-
-
-{-}
-toFoundations :: EOBoard -> EOBoard
-toFoundations board = toFoundations' board EOBoard {foundations = foundations board, columns = columns board, reserve = reserve board}
+moveToFoundation :: Card -> EOBoard -> EOBoard
+moveToFoundation card (fs,cs,rs) = (fs',cs',rs')
   where
-      --                temp       save to    output
-    toFoundations' :: EOBoard -> EOBoard -> EOBoard
-    toFoundations' board@(EOBoard fs cs rs) board'@(EOBoard fs' cs' rs')
-      -- if all cs checked, return board'
-      | null cs = board'
-      -- if no fs left to check, reset fs and try nxt c
-      | null fs = toFoundations' (EOBoard (fs') (tail cs) rs) board' 
-       --(EOBoard (fs' (init cs) rs)) board'
-      -- if f c valid, remove last of column and add to foundation, reset fs, next c
-      | foundationValid (last cs) (head fs) = toFoundations' (EOBoard fs' (tail cs) rs) (EOBoard (moveToFoundations fs' head fs (last cs)) (init . last cs) rs)
-      -- if f c not valid, remove f and try again
-      | otherwise = toFoundations' (EOBoard (init fs) cs rs) board'
--}
-{-}
+    fs' = if isAce card then [card] : fs else map (\f -> if matchSuit card (head f) then card : f else f) fs
+    cs' = map (delete card) cs
+    rs' = delete card rs
 
--}
-{-}
-foundationValid :: [Foundation] -> Column -> Bool
-foundationValid (f:fs) col
-  | null col = False
-  | isAce(tail col) && null fs = True
-  | pCard(tail col) == tail foundation = True
-  | null fs = False
-  | otherwise = foundationValid fs col
+matchSuit :: Card -> Card -> Bool
+matchSuit (s1,_) (s2,_) = s1 == s2
 
-foundationValid col f
-  | null col || null f = False
-  | isAce(tail col) && null f = True
-  | pCard(tail col) == tail f = True
-  | otherwise  = False
--}
--- iterate foundation suits
-
---foundations board
-
---data SBoard = 
