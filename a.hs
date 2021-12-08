960f3e0b2e8a812892c1e4c0064ce769f004c895
Zac Jones <63209111+zacjdev@users.noreply.github.com>
2021-12-08 01:58:02 +0000

    finish

--- a.hs
+++ a.hs
@@ -1,15 +1,16 @@
 {-
-Haskell Grading Assignment
-Author: Zac Jones (unless otherwise stated)
+COM2108- Haskell Grading Assignment
+Author: Zac Jones
 stack ghci --package random
 -}
 
--- Step 1: Initial Datatypes
+-- Imports
 import System.Random
 import Data.List
-import Data.Int (Int)
 import Data.Maybe
+import Debug.Trace
 
+-- Data types for Eight-off Solitaire
 data Suit = Hearts | Diamonds | Clubs | Spades
   deriving (Eq, Show, Ord, Enum, Bounded)
 
@@ -19,6 +20,45 @@ data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | 
 type Card = (Suit, Pip)
 
 type Deck = [Card]
+
+type Foundations = [Deck]
+
+type Columns = [Deck]
+
+type Reserves = Deck
+
+data Board = Board1 EOBoard | Board2 SBoard
+
+data EOBoard = EOBoard (Foundations, Columns, Reserves)
+
+instance Show EOBoard where
+  show (EOBoard (f, c, r)) = "Foundations:\n" ++ show f ++ "\nColumns:\n" ++ show c ++ "\nReserves:\n" ++ show r
+
+instance Show Board where
+  show (Board1 b) = show b
+  show (Board2 b) = show b
+
+
+-- Data types for Spider Solitaire
+
+data SpiCard = SpiCard (Suit, Pip, Bool)
+
+type SDeck = [SpiCard]
+
+type SColumns = [SDeck]
+
+type SFoundations = [SDeck]
+
+type Stock = SDeck
+
+data SBoard = SBoard (Stock , SColumns, SFoundations)
+
+instance Show SBoard where
+  show (SBoard (s, c, f)) = "Foundations:\n" ++ show f ++ "\nColumns:\n" ++ show c ++ "\nStock has " ++ show ((length s) `div` 10) ++ " deals left"
+
+instance Show SpiCard where
+  show (SpiCard (s, p, v)) = if v then show (s, p) else show "<unknown>"
+
 
 -- Step 2: Basic functionality
 pack :: [Card]
@@ -45,49 +85,8 @@ cmp (x1,y1) (x2,y2) = compare y1 y2
 
 -- Step 3: Types to represent a board
 
-type Foundations = [Deck]
-
-type Columns = [Deck]
-
-type Reserves = Deck
 
 
-data SCard = SCard (Suit, Pip, Bool)
-
-type SDeck = [SCard]
-
-makeHidden :: SCard -> SCard
-makeHidden (SCard (s, p, _)) = (SCard (s, p, False))
-
-makeVisible :: SCard -> SCard
-makeVisible (SCard (s, p, _)) = (SCard (s, p, True))
-
-
-instance Show SCard where
-  show (SCard (s, p, v)) = if v then show (s, p) else show "<unknown>"
-
-
-type SColumns = [SDeck]
-
-type SFoundations = [SDeck]
-
-type Stock = SDeck
-
-data SBoard = SBoard (Stock , SColumns, SFoundations)
-
-instance Show SBoard where
-  show (SBoard (s, c, f)) = "Foundations:\n" ++ show f ++ "\nColumns:\n" ++ show c ++ "\nStock has " ++ show ((length s) `div` 10) ++ " deals left"
-
-data Board = Board1 EOBoard | Board2 SBoard
-
-data EOBoard = EOBoard (Foundations, Columns, Reserves)
-
-instance Show EOBoard where
-  show (EOBoard (f, c, r)) = "Foundations:\n" ++ show f ++ "\nColumns:\n" ++ show c ++ "\nReserves:\n" ++ show r
-
-instance Show Board where
-  show (Board1 b) = show b
-  show (Board2 b) = show b
 
 
 compEOBoard :: Board
@@ -100,7 +99,7 @@ eODeal n = Board1 (EOBoard (fs, cs, rs))
   where
     deck = shuffle n pack
     fs = []
-    rs = take 4 deck
+    rs = drop 48 deck
     makeCols :: [Card] -> [Deck]
     -- split deck into decks of 6 cards
     makeCols xs = if length xs < 6 then [] else take 6 xs : makeCols (drop 6 xs)
@@ -113,10 +112,15 @@ findAces :: Board -> Deck
 findAces (Board1 (EOBoard (fs, cs, rs))) = filter isAce (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))
 
 findSuccs :: Board -> Deck
-findSuccs (Board1 (EOBoard (fs, cs, rs))) = filter (\f -> f `elem` (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))) (filter (not.null) (map sCard (map (\f -> last f) fs)))
+findSuccs (Board1 (EOBoard (fs, cs, rs)))
+  | length fs == 0 = []
+  | otherwise = trace ("SUCCS") (filter (\f -> f `elem` (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))) (filter (not.null) (map sCard (map (\f -> last f) (filter (not.null) fs)))))
 
 possibleMoveableCards :: Board -> [Card]
-possibleMoveableCards (Board1 (EOBoard (fs, cs, rs))) = map last (filter (not.null) cs) ++ rs
+possibleMoveableCards (Board1 (EOBoard (fs, cs, rs))) = (map last (filter (not.null) cs)) ++ rs
+
+possibleMoveableCardsNoReserves :: Board -> [Card]
+possibleMoveableCardsNoReserves (Board1 (EOBoard (fs, cs, rs))) = (map last (filter (not.null) cs))
 
 toFoundations :: Board -> Board
 toFoundations (Board1 (EOBoard board))
@@ -141,18 +145,28 @@ matchSuit (s1,_) (s2,_) = s1 == s2
 -- Step 5: Datatypes for spider solitaire
 
 
-sPack :: [SCard]
-sPack = [SCard (s, p, v) | s <- [Hearts .. Spades], p <- [Ace .. King], v <- [True]]
-
-
 -- Part 6: Function to deal an opening SBoard
+
+sPack :: [SpiCard]
+sPack = [SpiCard (s, p, v) | s <- [Hearts .. Spades], p <- [Ace .. King], v <- [True]]
+
+
+makeHidden :: SpiCard -> SpiCard
+makeHidden (SpiCard (s, p, _)) = (SpiCard (s, p, False))
+
+makeVisible :: SpiCard -> SpiCard
+makeVisible (SpiCard (s, p, _)) = (SpiCard (s, p, True))
+
+
+
+
 sDeal :: Int -> SBoard
 sDeal n = SBoard (stock, columns, foundations)
   where
     deck = shuffle n sPack ++ shuffle (n+1) sPack
     foundations = []
     stock = take 50 deck
-    makeSCols :: [SCard] -> [SDeck]
+    makeSCols :: [SpiCard] -> [SDeck]
     -- split deck into decks of 6 cards. 4x6cards, 6x5cards
     makeSCols xs
       | length xs < 6 = []
@@ -184,22 +198,26 @@ isNotAceKing _ = True
 
 -- execute boardFromCardMoveToColumn on each result from cardsThatCanMoveToColumn
 findMoves :: Board -> [Board]
-findMoves board = map (boardFromCardMoveToColumn board) (cardsThatCanMoveToColumn board) ++ map (boardFromKingMoveToEmptyColumn board) (kingsThatCanMoveToEmptyColumn board) ++ map (boardFromCardMoveToReserve board) (cardsThatCanMoveToReserve board)
-
+findMoves (Board1 (EOBoard (fs, cs, rs)))
+  | (not.null) (cardsThatCanMoveToColumn (Board1 (EOBoard (fs, cs, rs)))) = (map (boardFromCardMoveToColumn (Board1 (EOBoard (fs, cs, rs)))) (cardsThatCanMoveToColumn (Board1 (EOBoard (fs, cs, rs)))))
+  | (not.null) (kingsThatCanMoveToEmptyColumn (Board1 (EOBoard (fs, cs, rs)))) = (map (boardFromKingMoveToEmptyColumn (Board1 (EOBoard (fs, cs, rs)))) (kingsThatCanMoveToEmptyColumn (Board1 (EOBoard (fs, cs, rs)))))
+  | (not.null) (cardsThatCanMoveToReserve (Board1 (EOBoard (fs, cs, rs)))) = (map (boardFromCardMoveToReserve (Board1 (EOBoard (fs, cs, rs)))) (cardsThatCanMoveToReserve (Board1 (EOBoard (fs, cs, rs)))))
+  --length rs < 8 = map (boardFromCardMoveToReserve (Board1 (EOBoard (fs, cs, rs)))) (bestCardInColToMoveToRes (Board1 (EOBoard (fs, cs, rs))) : cardsThatCanMoveToReserve (Board1 (EOBoard (fs, cs, rs))))
+  | otherwise = []
 
 cardsThatCanMoveToColumn :: Board -> [Card]
-cardsThatCanMoveToColumn (Board1 (EOBoard (fs, cs, rs))) = filter isNotAceKing (filter (\c -> c `elem` (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))) (filter (not.null) (map pCard (map (\c -> last c) cs))))
+cardsThatCanMoveToColumn (Board1 (EOBoard (fs, cs, rs))) = filter isNotAceKing (filter (\c -> c `elem` (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))) (filter (not.null) (map pCard (map (\c -> last c) (filter (not.null)cs)))))
 
 boardFromCardMoveToColumn :: Board -> Card -> Board
 boardFromCardMoveToColumn (Board1 (EOBoard (fs, cs, rs))) card = (Board1 (EOBoard (fs', cs', rs')))
   where
-    cs' = map (\c -> if sCard card == (last c) then c ++ [card] else c) (map (delete card) cs)
+    cs' = map (\c -> if length c == 0 then c else if sCard card == (last c) then c ++ [card] else c) (map (delete card) cs)
     rs' = delete card rs
     fs' = fs
 
 kingsThatCanMoveToEmptyColumn :: Board -> [Card]
 kingsThatCanMoveToEmptyColumn (Board1 (EOBoard (fs, cs, rs)))
-  | (not.null) (filter (null) cs) = filter isKing (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))
+  | (not.null) (filter (null) ((filter (\c -> not (isKing (last c) && length c == 1)) (filter (not.null) cs)))) = filter isKing (possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))))
   | otherwise = []
 
 boardFromKingMoveToEmptyColumn :: Board -> Card -> Board
@@ -211,23 +229,16 @@ boardFromKingMoveToEmptyColumn (Board1 (EOBoard (fs, cs, rs))) card = (Board1 (E
 
 cardsThatCanMoveToReserve :: Board -> [Card]
 cardsThatCanMoveToReserve (Board1 (EOBoard (fs, cs, rs)))
-  | length rs < 8 = possibleMoveableCards(Board1 (EOBoard (fs, cs, rs)))
+  | length rs < 8 = possibleMoveableCardsNoReserves(Board1 (EOBoard (fs, (filter (\c -> if length c == 1 then True else if length c == 2 then not (sCard (last c) == head c) else not (sCard (last c) == c!!((length c - 2)))) (filter (not.null)cs )), rs)))
   | otherwise = []
 
 boardFromCardMoveToReserve :: Board -> Card -> Board
 boardFromCardMoveToReserve (Board1 (EOBoard (fs, cs, rs))) card = (Board1 (EOBoard (fs', cs', rs')))
   where
   cs' = map (delete card) cs
-  rs' = if length rs < 8 then card : (delete card rs) else rs
+  rs' = card : (delete card rs)
   fs' = fs
 
--- Move king to empty
--- Filter for boards after moving last of column to a foundation where a move could happen
--- If none, filter for boards aafter moving last 2, then 3, etc of column to foundatiton could do something useful
-
-
-
---chooseMove should ignore the "bad" moves
 chooseMove :: Board -> Maybe Board
 chooseMove board = if (not.null) (findMoves board) then Just (head (findMoves board)) else Nothing
 
@@ -237,85 +248,45 @@ haveWon board = (calculateScore board >= 52)
 -- Initial board, uses chooseMove to play the game to completion. Returns score
 playSolitaire :: Board -> Int
 playSolitaire board
-  | (chooseMove board) == Nothing = calculateScore board
-  | otherwise = playSolitaire (fromJust (chooseMove board))
-
-calculateScore :: Board -> Int
-calculateScore (Board1 (EOBoard (fs, cs, rs))) = sum (map length fs)
+  | isNothing (chooseMove board) = calculateScore board
+  | otherwise = trace ("debugging\n" ++ show board) (pS (toFoundations(fromJust (chooseMove board))))
 
 -- seed, games to play, return 
-analyseEO :: Int -> Int -> (Int, Int)
-analyseEO seed games = (wins, winrate)
+analyseEO :: Int -> Int -> (Int, Double)
+analyseEO seed games = (wins, avgscore)
   where
-    wins = length (filter (==52) (map playSolitaire (map eODeal [1..games])))
-    winrate = (wins * 100) `div` games
-
--- divide two ints
-divide :: Int -> Int -> Int
-divide x y = x `div` y
-
-{-}
-
--- iterate, see if the card can move to a column
-
-
--}
-
-
-
--- if yes, get resulting board and add it
-
--- stack ghci --package random
--- stack ghci --package random
-
-
-
-
--- cards that can move to a foundation function
--- move kings to empty column first
-
-
---boardsFromMoveToReserve :: Board -> [Board]
---boardsFromMoveToReserve (Board (EOBoard (fs, cs, rs))) = []
----- all possible combinations
--- no moving solo king
-
-
-
--- stack ghci --package random
+    scores = map playSolitaire (map eODeal [seed..seed+games])
+    avgscore = (fromIntegral (sum scores)) / (fromIntegral games)
+    wins = length (filter (==52) scores)
 
 {-
--- Part 2, Step 2: Function to choose next move
-chooseMove :: Board -> Maybe Board
-chooseMove b
-  | null findMoves b = Nothing
-  | otherwise = chooseMove' findMoves b
-    where
-      chooseMove' :: [Board] -> Board -> Maybe Board
-      chooseMove' [] _ = Nothing
-      chooseMove' (x:xs) b
-        | x == b = Just x
-        | otherwise = chooseMove' xs b
+Code designed to choose the best card to move to a reserve.
 
--- if board == findmoves board = nothing
+bestCardInColToMoveToRes :: Board -> Card
+bestCardInColToMoveToRes board = bestCardToReserve' board 1
+  where bestCardToReserve' :: Board -> Int -> Card
+        bestCardToReserve' (Board1 (EOBoard (fs, cs, rs))) n
+          -- give up, make random move
+          | n > 3 = (last (head cs))
+          | (not.null) (cardsWithMovesUnder (Board1 (EOBoard (fs, cs, rs))) n) = findCardToMoveFromCol (Board1 (EOBoard (fs, cs, rs))) (head (cardsWithMovesUnder (Board1 (EOBoard (fs, cs, rs))) n))
+          | otherwise = bestCardToReserve' (Board1 (EOBoard (fs, cs, rs))) (n+1)
 
+cardsWithMovesUnder :: Board -> Int -> [Card]
+cardsWithMovesUnder board n = map fst (filter (\a -> (snd a) == True) (zip (possibleMoveableUnder board n) (map (checkCardUnder board) (possibleMoveableUnder board n))))
 
+findCardToMoveFromCol :: Board -> Card -> Card
+findCardToMoveFromCol (Board1 (EOBoard (fs, cs, rs))) card = last (head (filter (\c -> card `elem` c) cs))
 
--- Part 2, Step 3: Function to play game of Eight-off
-haveWon :: Board -> Bool 
--- equal compBoard
-
-playSolitaire :: Board -> Int
-
--- may not be needed?
-countFoundations :: Board -> Int
-
--- Part 2, Step 4: Analyse performance
-
--- initial seed (iterate each time), number of games, return number of wins
-analyseEO :: Int -> Int -> Int
+checkCardUnder :: Board -> Card -> Bool
+checkCardUnder (Board1 (EOBoard (fs, cs, rs))) card
+-- is an empty column and the card is a king
+  | (not.null) (filter (null) cs) && isKing card = True
+-- card preceeds another card in a column
+  | sCard card `elem` possibleMoveableCards(Board1 (EOBoard (fs, cs, rs))) = True
+  | card `elem` (map sCard (map (\f -> last f) (filter (not.null) fs))) = True
+  | otherwise = False
 
 
--- Part 2, Step 5: Implement spider solitaire
-
+possibleMoveableUnder :: Board -> Int -> Deck
+possibleMoveableUnder (Board1 (EOBoard (fs, cs, rs))) layer = (map (\c -> c!!(length c - layer)) (filter (\c -> length c >= layer) cs))
 -}
